d8f689e9ecb8d551cf1c421c0ed7d4236623ed99
Atharva Keluskar <keluskaratharva999@gmail.com>
2025-07-27 14:28:42 +0530

    fix: set favicon and metadata title

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,29 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { Toaster } from 'react-hot-toast';
+import Navbar from "./components/Navbar";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Turf Booking | Book Your Turf Now",
+  icons: {
+    icon: "/logo.png", // favicon
+  },
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="/logo.png" />
-        <title>Turf Booking | Book Your Turf Now</title>
-      </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <Navbar />
         {children}
         <Toaster
